3b0154e13eaf07a9007c4a7af4b8bc42a56e1ca4
Gerard <gerhoquei8@gmail.com>
2023-05-15 17:29:12 +0200

    chicken logic

--- Assets/Assets/Chicken/ChickenLogic.cs
+++ Assets/Assets/Chicken/ChickenLogic.cs
@@ -6,10 +6,16 @@ public class ChickenLogic : MonoBehaviour
 {
     public int damage = 10;
     public int attackSpeed = 2;
+    public float attackSpeedTimer = 2;
+
     public bool attacking = false;
 
     private GameObject target;
     private Animator animator;
+
+    private float attackDuration = 0;
+
+    public float life = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +27,47 @@ public class ChickenLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, target.transform.position) < 2)
+        if(Vector3.Distance(transform.position, target.transform.position) <= 2)
         {
-            attacking = true;
-            Debug.Log("Attacking");
-            animator.SetBool("Walk", false);
-            animator.SetBool("Run", false);
+
+            if (attackSpeedTimer >= attackSpeed)
+            {
+                attacking = true;
+                Debug.Log("Attacking");
+                animator.SetBool("Walk", false);
+                animator.SetBool("Run", false);
+                animator.SetBool("Eat", attacking);
+
+                //animator.GetCurrentAnimatorStateInfo(0).IsName("Eat");
+                /*if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Eat"))
+                {
+                    attackSpeedTimer = 0;
+                }*/
+                attackDuration += 1 * Time.deltaTime;
+                if (attackDuration >= 1.5f)
+                {
+                    attackSpeedTimer = 0;
+                    attackDuration = 0;
+                }
+
+
+                Debug.Log(animator.GetCurrentAnimatorStateInfo(0).IsName("Eat"));
+            }
+            else if(attackSpeedTimer < attackSpeed)
+            {
+                attacking = false;
+                attackSpeedTimer += 1 * Time.deltaTime;
+                animator.SetBool("Eat", attacking);
+
+            }
+
+        }
+        else if (Vector3.Distance(transform.position, target.transform.position) > 2)
+        {
+
+            attacking = false;
             animator.SetBool("Eat", attacking);
+
         }
     }
 }
