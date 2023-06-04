581a77c0840cb596efd6d2c8c4d2c10b3cea9e55
Roger <rogersalas2000@gmail.com>
2023-06-04 18:09:46 +0200

    attacks different

--- Assets/Scripts/WandsAtack.cs
+++ Assets/Scripts/WandsAtack.cs
@@ -9,7 +9,7 @@ public class WandsAtack : MonoBehaviour
     public Rigidbody _BulletWater;
     public Rigidbody _BulletMeteore;
     public Rigidbody _BulletFire;
-    Select_Spell Select_Spell;
+    public Select_Spell Select_Spell;
     public GameObject _startPos;
     public float _speed;
    
@@ -22,10 +22,34 @@ public class WandsAtack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("space") && Select_Spell._stoneWand == Select_Spell._currrentSpell)
+        if (Input.GetKeyDown(KeyCode.Space) && Select_Spell._stoneWand == Select_Spell._currrentSpell)
           { 
             //Rigidbody createBullet = Instantiate(_BulletMeteore, _startPos.transform.position, transform.rotation) as Rigidbody;
             Rigidbody createBullet = Instantiate(_BulletMeteore, _startPos.transform.position, transform.rotation) as Rigidbody;
+
+            createBullet.velocity = transform.TransformDirection(new Vector3(0, _speed, 0));
+            //Physics.IgnoreCollision(createBullet.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && Select_Spell._waterWand == Select_Spell._currrentSpell)
+        {
+            //Rigidbody createBullet = Instantiate(_BulletMeteore, _startPos.transform.position, transform.rotation) as Rigidbody;
+            Rigidbody createBullet = Instantiate(_BulletWater, _startPos.transform.position, transform.rotation) as Rigidbody;
+
+            createBullet.velocity = transform.TransformDirection(new Vector3(0, _speed, 0));
+            //Physics.IgnoreCollision(createBullet.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && Select_Spell._windWand == Select_Spell._currrentSpell)
+        {
+            //Rigidbody createBullet = Instantiate(_BulletMeteore, _startPos.transform.position, transform.rotation) as Rigidbody;
+            Rigidbody createBullet = Instantiate(_BulletTornado, _startPos.transform.position, transform.rotation) as Rigidbody;
+
+            createBullet.velocity = transform.TransformDirection(new Vector3(0, _speed, 0));
+            //Physics.IgnoreCollision(createBullet.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && Select_Spell._fireWand == Select_Spell._currrentSpell)
+        {
+            //Rigidbody createBullet = Instantiate(_BulletMeteore, _startPos.transform.position, transform.rotation) as Rigidbody;
+            Rigidbody createBullet = Instantiate(_BulletFire, _startPos.transform.position, transform.rotation) as Rigidbody;
 
             createBullet.velocity = transform.TransformDirection(new Vector3(0, _speed, 0));
             //Physics.IgnoreCollision(createBullet.GetComponent<Collider>(), GetComponent<Collider>());
